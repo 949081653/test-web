cf863a383ac04e6bc40127f67f26e8e89cc4b74a
刘大德 <949081653@qq.com>
2016-04-10 18:29:02 +0800

    测试项目

---
+++ test-common/src/main/java/com/ldd/common/base/controller/BaseController.java
@@ -0,0 +1,13 @@
+package com.ldd.common.base.controller;
+
+/**
+ * <p>功能描述：</p>
+ * <p>Copyright: Copyright (c) 2014</p>
+ * <p>Company: 丞风智能科技有限公司</p>
+ *
+ * @author Mr.Liu
+ * @version 1.0 2016/4/6 19:30
+ */
+public class BaseController {
+
+}
